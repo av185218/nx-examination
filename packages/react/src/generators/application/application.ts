samarpanda/nx

feat(react): add support for React 19 for new Workspaces

packages/react/src/generators/application/application.ts
@@ -313,7 +313,7 @@ export async function applicationGeneratorInternal(
 
   // Handle tsconfig.spec.json for jest or vitest
   updateSpecConfig(host, options);
-  const stylePreprocessorTask = installCommonDependencies(host, options);
+  const stylePreprocessorTask = await installCommonDependencies(host, options);
   tasks.push(stylePreprocessorTask);
   const styledTask = addStyledModuleDependencies(host, options);
   tasks.push(styledTask);

av185218/nx-examination

packages/react/src/generators/application/application.ts
@@ -151,7 +151,7 @@ export async function applicationGeneratorInternal(
 
   // Handle tsconfig.spec.json for jest or vitest
   updateSpecConfig(tree, options);
-  const stylePreprocessorTask = installCommonDependencies(tree, options);
+  const stylePreprocessorTask = await installCommonDependencies(tree, options);
   tasks.push(stylePreprocessorTask);
   const styledTask = addStyledModuleDependencies(tree, options);
   tasks.push(styledTask);